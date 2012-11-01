451295fae0603bb145357337607fc74cd1cfb8ce
Erik <etinlb@gmail.com>
2012-11-01 16:26:52 -0700

    made a branch

--- trades/models.py
+++ trades/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Trades(models.Model):
-  tradesId = models.IntegerField()
+  #tradesId = models.IntegerField()
   status = models.CharField(max_length=10)
-  rating = models.CharField(max_length=10)
+  rating = models.ChoiceField(choices=(1, 2, 3, 4, 5))
   user1Id = models.IntegerField()
   user2Id = models.IntegerField()
   game1Id = models.IntegerField()
