746e93f03731e5624bb0a9e26930fe005bc0bfdc
Erik <etinlb@gmail.com>
2012-11-06 12:01:52 -0800

    Added a search module

--- trades/models.py
+++ trades/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Trades(models.Model):
   #tradesId = models.IntegerField()
   status = models.CharField(max_length=10)
-  rating = models.ChoiceField(choices=(1, 2, 3, 4, 5))
+  #rating = models.ChoiceField(choices=(1, 2, 3, 4, 5))
   user1Id = models.IntegerField()
   user2Id = models.IntegerField()
   game1Id = models.IntegerField()
