00bd509daa7113129769daff93db6cf1c097f22a
Leo Liu <leo367322@gmail.com>
2012-11-06 11:25:18 -0800

    database models.py

--- urls.py
+++ urls.py
@@ -3,6 +3,9 @@ from django.conf.urls.defaults import *
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
+    (r'^trades/$', 'trades.views.index'),
+	(r'^trades/save/(?P<users_name>\w+)/$', 'trades.views.save'),
+	(r'^trades/load/(?P<users_id>\d+)/$', 'trades.views.load'),
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     ('^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'home.html'}),
