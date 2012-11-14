1b41d0cc95078d8fffd007701d88d8abf4a43c84
Cesar de la Vega <cesarvegaro@gmail.com>
2012-11-13 17:48:29 -0800

    Adds sign view

--- trades/views.py
+++ trades/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 from trades.models import UserProfile
 
@@ -7,7 +8,7 @@ def index(request):
     user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
     user_profile = UserProfile(user = user, account='account', address='address', rating=1)
     user_profile.save()
-    return HttpResponse("")
+    return render_to_response('users/sign.html',)
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the trades index.")
