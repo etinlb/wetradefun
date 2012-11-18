eba9a1101d4337a78a4ac9f25d55a8ed311b123b
Cesar de la Vega <cesarvegaro@gmail.com>
2012-11-18 11:36:54 -0800

    Adds messages. Adds sign up page

--- trades/urls.py
+++ trades/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
-    ('^$', 'trades.views.sign'),
+    ('^$', 'trades.views.sign_up'),
+    ('^users/sign_in$', 'trades.views.sign_up'),
 )
--- trades/views.py
+++ trades/views.py
@@ -3,6 +3,8 @@ import datetime, random, sha
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.contrib import messages
 
 from trades.forms import RegistrationForm
 
@@ -10,7 +12,7 @@ from trades.models import *
 import search as s
 from trades.forms import SearchForm
 
-def sign(request):
+def sign_up(request):
     if request.method == 'POST': # If the form has been submitted...
         form = RegistrationForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
@@ -21,13 +23,20 @@ def sign(request):
                 form.cleaned_data['password'],)
             user_profile = UserProfile(user = user, account='account', address='address', rating=1)
             user_profile.save()
-            return HttpResponse("You save a user. Please load his name by using id %s.")
+            messages.add_message(request, messages.SUCCESS, 'Thanks for registering %s' % user.username)
+            # Login the user
+            login(request, user)
+            # Send to home page
+
+        else:
+            messages.add_message(request, messages.ERROR, form._errors['__all__'])
     else:
         form = RegistrationForm() # An unbound form
 
     return render_to_response('users/sign.html', {
         'form': form,
-    })
+    },
+     context_instance=RequestContext(request))
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the trades index.")
