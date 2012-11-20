4aee5c898d10dd6772a0d642554ebfab87b5059f
Andre So <andrektso@gmail.com>
2012-11-20 11:16:51 -0800

    ASDA

--- user/models.py
+++ user/models.py
@@ -1,3 +1,61 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+  user = models.ForeignKey(User, unique=True)
+  account = models.CharField(max_length=64)
+  # password = models.CharField(max_length=64)
+  # name = models.CharField(max_length=64)
+  # email = models.CharField(max_length=64)
+  address = models.CharField(max_length=64)
+  rating = models.IntegerField()
+  # dateRegistered = models.IntegerField()
+
+class Game(models.Model):
+  name = models.CharField(max_length=64)
+  gianBombID = models.IntegerField()
+  rating = models.IntegerField()
+
+class Wishlist(models.Model):
+  userID = models.ForeignKey(UserProfile)
+  gameID = models.ForeignKey(Game)
+  datePosted = models.IntegerField()
+
+class Currentlist(models.Model):
+  status = models.CharField(max_length=64)
+  datePosted = models.IntegerField()
+  datePosted = models.IntegerField(default=-1)
+  userID = models.ForeignKey(UserProfile)
+  gameID = models.ForeignKey(Game)
+
+class Transaction(models.Model):
+  status = models.CharField(max_length=64)
+  dateRequested = models.IntegerField()
+  dateTraded = models.IntegerField()
+  senderID = models.ForeignKey(UserProfile)
+  senderGameID = models.ForeignKey(Game)
+  receiverID = models.ForeignKey(UserProfile)
+  receiverGameID = models.ForeignKey(Game)
+
+class Gamecomment(models.Model):
+  content = models.CharField(max_length=64)
+  userID = models.ForeignKey(UserProfile)
+  gameID = models.ForeignKey(Game)
+  datePosted = models.IntegerField()
+
+class Message (models.Model):
+  content = models.CharField(max_length=128)
+  datePosted = models.IntegerField()
+  senderID = models.ForeignKey(UserProfile)
+  receiverID = models.ForeignKey(UserProfile)
+  transactionID = models.ForeignKey(Transaction)
+
+class Userrating (models.Model):
+  rating = models.IntegerField()
+  senderID = models.ForeignKey(UserProfile)
+  receiverID = models.ForeignKey(UserProfile)
+
+class Gamerating (models.Model):
+  rating = models.IntegerField()
+  userID = models.ForeignKey(UserProfile)
+  gameID = models.ForeignKey(Game)
--- user/views.py
+++ user/views.py
@@ -1,1 +1,54 @@
 # Create your views here.
+
+import datetime, random, sha
+
+
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import Context, loader, RequestContext
+from django.contrib import messages
+
+from user.forms import *
+
+
+handler500 = 'djangotoolbox.errorviews.server_error'
+
+def index(request):
+    return HttpResponse('THIS MESSAGE WILL HAVE TO DO UNTIL I FIGURE OUT HOW TO WORK TEMPLATES PROPERLY')
+
+
+def log_in(request):
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
+		if form.is_valid():
+    		user = authenticate(username=usern, password=passw)
+    		if user is not None:
+        		if user.is_active:
+            		login(request, user)
+            		return HttpResponse("I GO TO SUCCESSPAGE")
+            		
+            	else:
+            		return HttpResponse("LOL UR ACC DISABLED")
+
+   			else:
+        		return render_to_response('users/sign.html', {
+        		'form': form,
+    			},
+    			context_instance=RequestContext(request))	
+        else: 
+            if "__all__" in form._errors:
+                messages.add_message(request, messages.ERROR, form._errors['__all__'])
+
+    else:
+    	form = LoginForm()
+
+def save(request, users_name):
+    u=UserProfile(name=users_name)
+    u.save()
+    return HttpResponse("You save a user. Please load his name by using id %s." % u.id)
+
+def load(request, users_id):
+    u=Users.objects.get(id=users_id)
+    return HttpResponse("You load a user whose name is %s." % u.name)
+
