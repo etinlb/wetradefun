e25909f25ba2c04ab61f2159c6b7db9d7b00ef37
Anurag Kalavakunta <akalavakunta@gmail.com>
2012-11-20 12:29:11 -0800

    Added stuff to views in user

--- user/views.py
+++ user/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+import datetime, random, sha
+
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.contrib import messages
+
+from trades.forms import RegistrationForm
+
+from trades.models import *
+import search as s
+from trades.forms import SearchForm
+
+def account_management(request, userID)
+	try:
+      user_profile = UserProfile.objects.get(id = userID)
+  except Currentlist.DoesNotExist:
+      user_profile = None
+
+  
