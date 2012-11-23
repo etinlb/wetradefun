856842e9a874d8c08ba7fac2ca53ffd45e01c341
Andre So <andrektso@gmail.com>
2012-11-22 23:52:55 -0800

    testing 11/22/12

--- user/models.py
+++ user/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
+# from trades.models import Transaction 
 
+# Create your models here.
 class UserProfile(models.Model):
   user = models.ForeignKey(User, unique=True)
   account = models.CharField(max_length=64)
@@ -11,36 +13,9 @@ class UserProfile(models.Model):
   rating = models.IntegerField()
   # dateRegistered = models.IntegerField()
 
-class Game(models.Model):
-  name = models.CharField(max_length=64)
-  gianBombID = models.IntegerField()
-  rating = models.IntegerField()
-
 class Wishlist(models.Model):
   userID = models.ForeignKey(UserProfile)
-  gameID = models.ForeignKey(Game)
-  datePosted = models.IntegerField()
-
-class Currentlist(models.Model):
-  status = models.CharField(max_length=64)
-  datePosted = models.IntegerField()
-  datePosted = models.IntegerField(default=-1)
-  userID = models.ForeignKey(UserProfile)
-  gameID = models.ForeignKey(Game)
-
-class Transaction(models.Model):
-  status = models.CharField(max_length=64)
-  dateRequested = models.IntegerField()
-  dateTraded = models.IntegerField()
-  senderID = models.ForeignKey(UserProfile)
-  senderGameID = models.ForeignKey(Game)
-  receiverID = models.ForeignKey(UserProfile)
-  receiverGameID = models.ForeignKey(Game)
-
-class Gamecomment(models.Model):
-  content = models.CharField(max_length=64)
-  userID = models.ForeignKey(UserProfile)
-  gameID = models.ForeignKey(Game)
+  #gameID = models.ForeignKey(Game)
   datePosted = models.IntegerField()
 
 class Message (models.Model):
@@ -48,14 +23,9 @@ class Message (models.Model):
   datePosted = models.IntegerField()
   senderID = models.ForeignKey(UserProfile)
   receiverID = models.ForeignKey(UserProfile)
-  transactionID = models.ForeignKey(Transaction)
+  transactionID = models.ForeignKey('trades.models', verbose_name=u'Transaction')
 
 class Userrating (models.Model):
   rating = models.IntegerField()
   senderID = models.ForeignKey(UserProfile)
   receiverID = models.ForeignKey(UserProfile)
-
-class Gamerating (models.Model):
-  rating = models.IntegerField()
-  userID = models.ForeignKey(UserProfile)
-  gameID = models.ForeignKey(Game)
--- user/views.py
+++ user/views.py
@@ -1,54 +1,89 @@
-# Create your views here.
-
-import datetime, random, sha
-
-
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+from user.forms import RegistrationForm, LoginForm
+
+from trades.models import *
+
+from django.http import HttpResponse, HttpResponseRedirect
+
 from django.shortcuts import render_to_response, get_object_or_404
-from django.template import Context, loader, RequestContext
+from django.template import RequestContext
 from django.contrib import messages
 
-from user.forms import *
+import search as s
 
 
-handler500 = 'djangotoolbox.errorviews.server_error'
 
-def index(request):
-    return HttpResponse('THIS MESSAGE WILL HAVE TO DO UNTIL I FIGURE OUT HOW TO WORK TEMPLATES PROPERLY')
+def sign_in(request):
+    # If it's 
+    if request.user.is_authenticated():
+      return HttpResponseRedirect('/')
+    else:
+      if request.method == 'POST': # If the form has been submitted...
+          form = LoginForm(request.POST) # A form bound to the POST data
+          if form.is_valid(): # All validation rules pass
+              username = form.cleaned_data['username']
+              password = form.cleaned_data['password']
+              user = authenticate(username=username, password=password)
+              if user is not None:
+                  if user.is_active:
+                    login(request, user)
+                    # Redirect to a success page.
+                    messages.add_message(request, messages.SUCCESS, 'Welcome %s!' % user.username)
+                    return HttpResponseRedirect('/')
+                  else:
+                    # Return a 'disabled account' error message
+                    messages.add_message(request, messages.ERROR, 'Your account is disabled')
+              else:
+                # Return an 'invalid login' error message.
+                messages.add_message(request, messages.ERROR, 'Your username or password is incorrect')
+      else:
+          form = LoginForm() # An unbound form
 
+    return render_to_response('users/sign_in.html', {
+        'form': form,
+    },
+     context_instance=RequestContext(request))
 
-def log_in(request):
-	if request.method == 'POST':
-		form = LoginForm(request.POST)
-		if form.is_valid():
-    		user = authenticate(username=usern, password=passw)
-    		if user is not None:
-        		if user.is_active:
-            		login(request, user)
-            		return HttpResponse("I GO TO SUCCESSPAGE")
-            		
-            	else:
-            		return HttpResponse("LOL UR ACC DISABLED")
+def account_management(request, userID):
+  try:
+    user_profile = UserProfile.objects.get(id = userID)
+  except Currentlist.DoesNotExist:
+    user_profile = None
 
-   			else:
-        		return render_to_response('users/sign.html', {
-        		'form': form,
-    			},
-    			context_instance=RequestContext(request))	
-        else: 
+  current_list = Currentlist.objects.get(user = userID)
+  wish_list = Wishlist.objects.get(user = userID)
+  trans_hist = Transaction.objects.get(status = "completed", sender = userID)
+  trans_hist2 = Transaction.objects.get(status = "completed", receiver = userID)
+
+def sign_up(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = RegistrationForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            user = User.objects.create_user(
+                form.cleaned_data['username'],
+                form.cleaned_data['email'],
+                form.cleaned_data['password'],)
+            user_profile = UserProfile(user = user, account='account', address='address', rating=1)
+            user_profile.save()
+            messages.add_message(request, messages.SUCCESS, 'Thanks for registering %s' % user.username)
+            # Login the user
+            login(request, user)
+            # Send to home page
+
+        else:
             if "__all__" in form._errors:
                 messages.add_message(request, messages.ERROR, form._errors['__all__'])
-
     else:
-    	form = LoginForm()
+        form = RegistrationForm() # An unbound form
 
-def save(request, users_name):
-    u=UserProfile(name=users_name)
-    u.save()
-    return HttpResponse("You save a user. Please load his name by using id %s." % u.id)
+    return render_to_response('users/sign.html', {
+        'form': form,
+    },
+     context_instance=RequestContext(request))
 
-def load(request, users_id):
-    u=Users.objects.get(id=users_id)
-    return HttpResponse("You load a user whose name is %s." % u.name)
 
+def account_management(request):
+    return render_to_response('users/account_management.html')
