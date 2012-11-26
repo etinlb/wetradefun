8a520626da7291063cdbfc6e790b3c8f807b780e
etinlb@gmail.com <etinlb@gmail.com>
2012-11-26 09:55:57 -0800

    Changed a bunch, check the log for details

--- trades/models.py
+++ trades/models.py
@@ -23,8 +23,14 @@ class Transaction(models.Model):
   dateRequested = models.DateTimeField(auto_now_add=True)
   dateTraded = models.CharField(max_length=64)
   sender = models.ForeignKey(UserProfile,related_name='Transaction_sender')
-  sender_giantBombID = models.IntegerField()
+  sender_game_id = models.ForeignKey('Game')
   receiver = models.ForeignKey(UserProfile,related_name='Transaction_receiver')
-  receiver_giantBombID = models.IntegerField()
+  receiver_game_id = models.ForeignKey('Game')
+
+class Game(models.Model):
+  platform = models.CharField(max_length=64)
+  image_url = models.CharField()
+  name = models.CharField()
+  giant_bomb_id = models.IntegerField()
 
 
