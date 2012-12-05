880be5fed57e551380a4e08d9d71e0e425a76d57
Anurag Kalavakunta <akalavakunta@gmail.com>
2012-12-04 20:33:00 -0800

    fixed deferring and added backend msgs

--- trades/views.py
+++ trades/views.py
@@ -120,6 +120,7 @@ def accept_offer(request):
   if request.is_ajax():
     transaction = Transaction.objects.get(pk = request.GET.get('transaction_id'))
     other_trans = Transaction.objects.filter(current_listing = transaction.current_listing)
+    r_message = request.get.GET('comment')
     if (transaction != None):
 
       for ot in other_trans:
@@ -129,6 +130,13 @@ def accept_offer(request):
 
       if (transaction.status == "offered" and already_accepted == False):
         transaction.status = "accepted"
+        transaction.receiver_message = r_message
+
+        for ot in other_trans:
+          if (ot.pk != transaction.pk and ot.status == "offered"):
+            ot.status = "deferred"
+            ot.save()
+
         messages.success(request, "You have successfully accepted the trade offer")
         transaction.save()
         mails.send(
@@ -227,13 +235,22 @@ def decline_offer(request):
 
 @login_required(login_url='/users/sign_in/')
 def delete_offer(request):
+  message = ""
+
   if request.is_ajax():
     userprofile = request.user.get_profile()    
     transaction = Transaction.objects.get(pk = request.GET.get('transaction_id'))
+    all_trans = Transaction.objects.filter(current_listing = transaction.current_listing)
+    
     if transaction != None:
       if ((userprofile == transaction.sender) and ((transaction.status == "offered") or (transaction.status == "accepted"))):
+        for ot in all_trans:
+          if (ot.pk != transaction.pk and ot.status == "deferred"):
+            ot.status = "offered"
+            ot.save() 
+
         transaction.delete()
-        #message = userprofile.user.username + " deleted the offer"
+        message = userprofile.user.username + " deleted the offer"        
       else:
         message="This trade is no longer available or has already been confirmed"
     else:
@@ -282,6 +299,7 @@ def make_offer(request):
       s_platform = request.GET.get('s_platform')
       s_game = get_game_table_by_id(request.GET.get('game1_id'), s_platform) # sender game / game offered
       r_game = get_game_table_by_id(request.GET.get('game2_id'), r_platform) # receiver game / game listed
+      s_message = request.GET.get('comment') # <---- CHANGE THIS BASED ON TEMPLATES
       if (s_game.giant_bomb_id == r_game.giant_bomb_id):
         messages.error(request, "These two games are the same games for the same platforms")
       else:
@@ -290,7 +308,7 @@ def make_offer(request):
             message = "Cannot offer a game to your own listing"
             messages.error(request,"You can't offer games to yourself. The offer you made to yourself will not be reflected")
           else:
-            transaction = Transaction.objects.create(status = "offered", sender = userprofile, sender_game = s_game, current_listing = listing)
+            transaction = Transaction.objects.create(status = "offered", sender = userprofile, sender_game = s_game, current_listing = listing, sender_message = s_message)
             transaction.save()
             mails.send(
               'Someone has made an offer for your game!',
@@ -298,8 +316,7 @@ def make_offer(request):
               listing.user.user.email, 
               listing.user.user.email, 
               'Good news! Someone has made an offer for your game ' + listing.game_listed.name + 
-              '\n\n http://wetradefun.appspot.com'     
-
+              '\n\n http://wetradefun.appspot.com'
               )
 
         messages.success(request, "You have made an offer for " + r_game.name + " for the " + r_game.platform)
