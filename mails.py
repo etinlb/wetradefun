b4b145389ffb6d2fa5078b9c4c15ddc6d8aac757
Ching-yao Liu <leoliu1313@gmail.com>
2012-12-07 17:54:48 -0800

    fix delete deffered transaction issue

--- mails.py
+++ mails.py
@@ -9,4 +9,4 @@ def send(subject, from_name, from_email, to_name, to_email, body):
   message.to = to_name + " <"+to_email+">"
   message.body = body
 
-  message.send # comment this line if testing on local machine
+  message.send # comment this line if testing on local machine 
